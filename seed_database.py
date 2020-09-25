a93d41641076db139dc3bb43641ef5e79119e46b
Kate Schlough <krschlough@gmail.com>
2020-09-25 21:40:45 +0000

    Completed seed_database.py

--- seed_database.py
+++ seed_database.py
@@ -17,3 +17,14 @@ model.db.create_all()
 
 with open('data/movies.json') as f:
     movie_data = json.loads(f.read())
+
+movies_in_db = []
+for movie in movie_data:
+    title = movie['title']
+    overview = movie['overview']
+    poster_path = movie['poster_path']
+    release_date = datetime.strptime(movie['release_date'], '%Y-%m-%d')
+
+    db_movie = crud.create_movie(title, overview, release_date, poster_path)
+
+    movies_in_db.append(db_movie)
